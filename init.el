73f166a90c6345e02d3613f60cbfef5eb56d5e66
kolemikko <mikko.kolehmainen@tuta.io>
2023-03-22 21:24:24 +0200

    Update Straight and rename obsolete variable

--- init.el
+++ init.el
@@ -14,14 +14,19 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(define-obsolete-variable-alias
+  'native-comp-deferred-compilation-deny-list
+  'native-comp-jit-compilation-deny-list
+  "Renamed in emacs#95692f6")
+
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
+      (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
