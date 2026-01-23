e71e0cd051d2a9db594e51eb110ee49f10f80244
Mikko Kolehmainen <mikko.kolehmainen@tuta.io>
2026-01-23 13:28:52 +0100

    Set Evil Mode arrow keys and respect visual lines properly

--- modules/evil-mode.el
+++ modules/evil-mode.el
@@ -1,9 +1,9 @@
 ;;; evil-mode.el -- All Evil-mode related configuration
 
-(setq evil-want-keybinding nil)
-
 (use-package evil
   :init
+  ;; Set before loading evil-collection
+  (setq evil-want-keybinding nil)
   (setq evil-undo-system 'undo-fu)
   (setq evil-want-C-u-scroll t)
   (setq evil-want-C-i-jump nil)
@@ -13,6 +13,16 @@
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
   (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
+  ;; Arrow keys behave like hjkl in normal/visual/motion states
+  (dolist (map (list evil-normal-state-map
+                     evil-visual-state-map
+                     evil-motion-state-map))
+    (define-key map (kbd "<left>")  #'evil-backward-char)
+    (define-key map (kbd "<down>")  #'evil-next-visual-line)
+    (define-key map (kbd "<up>")    #'evil-previous-visual-line)
+    (define-key map (kbd "<right>") #'evil-forward-char))
+
+  ;; Set initial states
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'pdf-view-mode 'motion))
 
